28ea23dbce2560df5433bd718eae951b637497fa
Prof. Dr. Arif Wider <wider@htw-berlin.de>
2025-10-06 13:09:18 +0200

    adds GET route

--- src/main/java/com/example/demo/TodoEntry.java
+++ src/main/java/com/example/demo/TodoEntry.java
@@ -5,4 +5,10 @@ import java.time.LocalDateTime;
 public class TodoEntry {
     String name;
     LocalDateTime due;
+
+    TodoEntry() {
+    }
+    public TodoEntry(String name) {
+        this.name = name;
+    }
 }
